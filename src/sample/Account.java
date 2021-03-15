9eeb8565248c68dd70c8b8f37a907e763380aaff
dave <vzm19uhu@uea.ac.uk>
2021-03-15 12:37:21 +0000

    Add hash function

--- src/sample/Account.java
+++ src/sample/Account.java
@@ -1,4 +1,49 @@
 package sample;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
+
 public class Account {
+    private static Account[] accounts;
+    private int userID;
+    private String password;
+    private String username;
+    private String email;
+
+    public Account(int userID, String password, String username, String email) {
+        this.userID = userID;
+        this.password = hashString(password);
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    //public Member getMember(){
+    //    return new Member();
+    //}
+    //Member not implemented yet
+
+    private static String hashString(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashed = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hashed);
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
 }
